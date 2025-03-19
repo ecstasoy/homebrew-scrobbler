5a1430abdb1ed4377521e1fd67881de4701e6381
Kunhua Huang <huang.kunh@northeastern.edu>
2025-03-19 12:14:39 +0800

    Update scrobbler.rb

--- Formula/scrobbler.rb
+++ Formula/scrobbler.rb
@@ -2,7 +2,7 @@ class Scrobbler < Formula
   desc "global macOS Last.fm Scrobbler with support for various music platforms"
   homepage "https://github.com/ecstasoy/BetterScrobbler"
   url "https://github.com/ecstasoy/BetterScrobbler/releases/download/v1.2.2/scrobbler-1.2.2.tar.gz"
-  sha256 "355d19ac637ce226f9dae96628e327d4308c166c90827c4f8672849f50750875"
+  sha256 "cdbd7a248e535eba467b6b5e1d18190f84f91e2c639814b0c6a8376ecf92199a"
   license "MIT"
 
   depends_on "cmake" => :build
